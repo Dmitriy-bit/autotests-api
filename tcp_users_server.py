1f3735a2734769ac217146e1b004b9ac50b31135
Dmitriy-bit <dmorfius@gmail.com>
2026-04-08 00:31:31 +0300

    5.7 Знакомство с TCP/IP протоколами. Практика работы с TCP/IP протоколом

---
+++ tcp_users_server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def server():
+    messages = []
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('127.0.0.1', 12345)
+    server_socket.bind(server_address)
+    server_socket.listen(10)
+    print("Сервер запущен и ждёт подключений...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Пользователь с адресом: {client_address} подключился к серверу")
+
+        message = client_socket.recv(1024).decode()
+        print(f"Пользователь с адресом: {client_address} отправил сообщение: {message}")
+        messages.append(message)
+        client_socket.send('\n'.join(messages).encode())
+
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    server()
