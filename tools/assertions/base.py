e4101c9cc4481ae747b72c1c1fe389296880ce63
Dmitriy-bit <dmorfius@gmail.com>
2026-06-16 12:39:00 +0300

    8.2 Написание API автотестов используя Pytest. Практика написания API автотестов

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -3,15 +3,15 @@ from typing import Any
 
 def assert_status_code(actual: int, expected: int):
     """
-    Проверяет, что бактический статус-код ответа соответствует ожидаемому•
+    Проверяет, что фактический статус-код ответа соответствует ожидаемому•
 
     :param actual: Фактический статус-код ответа.
-    :param expected: Ожидаемый статус код.
+    :param expected: Ожидаемый статус-код.
     :raises AssertionError: Если статус-коды не совпадают.
     """
     assert actual == expected, (
         'Incorrect response status code. ',
-        f'Expected status code: {expected}. '
+        f'Expected status code: {expected}. ',
         f'Actual status code: {actual}. ',
     )
 
